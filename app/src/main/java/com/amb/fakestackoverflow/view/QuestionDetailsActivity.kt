975f2ae6244ec946dfd3f6bd7f0aa597ff3be997
Arthur Morsoleto Batista <arthur.morsoleto@gmail.com>
2021-08-17 18:47:04 -0300

    add questions details activity

---
+++ app/src/main/java/com/amb/fakestackoverflow/view/QuestionDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.amb.fakestackoverflow.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.amb.fakestackoverflow.R
+
+class QuestionDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question_details)
+    }
+}
